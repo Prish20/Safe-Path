99d795fcb1202dfab99b64a641165d00cd6fd7ea
Christadrian Sanya <chrismadegwa3@gmail.com>
2024-10-22 04:33:37 +0300

    CleanUp the folder structure for a new layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,8 @@
-import { BrowserRouter } from 'react-router-dom';
-import Router from '@/routes';
-import { MediaQueryProvider } from '@/contexts/MediaQueryContext';
-import { AuthProvider } from '@/contexts/AuthContext';
-import ContainerLayout from '@/layouts/ContainerLayout';
-import AuthGuard from '@/components/AuthGuard';
 
-function App() {
+const App = () => {
   return (
-    <MediaQueryProvider>
-      <BrowserRouter>
-        <ContainerLayout>
-          <AuthProvider>
-            <AuthGuard>
-              <Router />
-              {/* <Toaster position="top-right" richColors closeButton /> */}
-            </AuthGuard>
-          </AuthProvider>
-        </ContainerLayout>
-      </BrowserRouter>
-    </MediaQueryProvider>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- client/src/lib/SERVERURL.ts
+++
@@ -1,4 +0,0 @@
-export const ServerURL = import.meta.env.PROD 
-  ? 'https://plantcare.mikegirma.tech'  // production
-  : 'http://localhost:3003';             // development
-console.log('ServerURL:', ServerURL);

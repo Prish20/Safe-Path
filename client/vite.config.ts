ea9035511934560c88d594feaecb4357ccf6d8ce
Christadrian Sanya <chrismadegwa3@gmail.com>
2024-11-04 22:39:23 +0300

    Minor updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 
-const apiURL = process.env.VITE_API_URL || "http://localhost:3000";
+const apiURL ="http://localhost:3000";
 
 // https://vitejs.dev/config/
 export default defineConfig({
